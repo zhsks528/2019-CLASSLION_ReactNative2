afcc2e34c629b61b3e6ee53a3658d962a995e3b7
zhsks528 <hsm950825@naver.com>
2019-12-27 18:05:12 +0900

    Input & 디자인 추가

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Dimensions, TextInput } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Writeheader from '../components/WriteHeader';
+
+const { width, height } = Dimensions.get('window');
 
 export default class WriteScreen extends Component {
+
   static navigationOptions = {
     tabBarIcon: ({ tintColor }) => (
       <MaterialCommunityIcons
@@ -20,7 +24,17 @@ export default class WriteScreen extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.textstyle}>Write Screen</Text>
+        <View style={styles.contentContainer}>
+        <Writeheader />
+        <TextInput
+          placeholder="제목을 입력하세요"
+          style={styles.title}
+          returnKeyType="done" />
+        <TextInput 
+          placeholder="내용을 입력하세요"
+          multiline={true}
+          style={styles.content} />
+        </View>
       </SafeAreaView>
     );
   }
@@ -29,11 +43,20 @@ export default class WriteScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems : 'center',
+    paddingTop : 50
   },
-  textstyle: {
-    fontSize: 40
+  contentContainer : {
+    width: width - 60,
+  },
+  title : {
+    marginVertical : 30,
+    fontSize : 30,
+    paddingBottom : 12,
+    borderBottomWidth : 2,
+  },
+  content : {
+    fontSize : 20
   }
+
 });
