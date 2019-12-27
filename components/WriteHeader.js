6681f49315e5cb1640bb3a50a677af2c43a06fa4
zhsks528 <hsm950825@naver.com>
2019-12-27 18:04:51 +0900

    WriteHeader Components 생성

---
+++ components/WriteHeader.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { withNavigation } from "react-navigation";
+
+const WriteHeader = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.goBack();
+        }}
+        hitSlop={{ top: 32, bottom: 32, left: 32, right: 32 }}
+      >
+        <Ionicons name="ios-arrow-back" size={25} />
+      </TouchableOpacity>
+
+      <View style={styles.iconContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+          hitSlop={{ top: 2, bottom: 2, left: 2, right: 2 }}
+        >
+          <Ionicons name="ios-image" size={25} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+          hitSlop={{ top: 2, bottom: 2, left: 2, right: 2 }}
+        >
+          <Ionicons name="ios-save" size={25} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  iconContainer: {
+    flexDirection: "row",
+    width: 60,
+    justifyContent: "space-between"
+  }
+});
+
+export default withNavigation(WriteHeader);
