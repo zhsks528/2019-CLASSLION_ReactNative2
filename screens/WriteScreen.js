5d51af9db1257206ee8ac37281dcd2da3e964622
zhsks528 <hsm950825@naver.com>
2019-12-27 16:47:59 +0900

    WriteScreen 생성

---
+++ screens/WriteScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-navigation";
+
+export default function WriteScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textstyle}>Write Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textstyle: {
+    fontSize: 40
+  }
+});
