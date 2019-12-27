9037da035367a7225116b81030d590642b07365b
zhsks528 <hsm950825@naver.com>
2019-12-27 16:48:18 +0900

    BaseNavi 추가

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
+import MainScreen from "./screens/MainScreen";
+import DetailScreen from "./screens/DetailScreen";
+import WriteScreen from "./screens/WriteScreen";
 
+const BaseNavi = createBottomTabNavigator({
+  MainScreen: {
+    screen: MainScreen
+  },
+  DetailScreen: {
+    screen: DetailScreen
+  },
+  WriteScreen: {
+    screen: WriteScreen
+  }
+});
+
+const MyNavi = createAppContainer(BaseNavi);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyNavi />
     </View>
   );
 }
@@ -12,8 +33,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff"
+  }
 });
