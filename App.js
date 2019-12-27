eb3a7ec8e3f3f6c4d6f009cd4b83a9404b01d91e
zhsks528 <hsm950825@naver.com>
2019-12-27 17:08:17 +0900

    Icon 추가

--- App.js
+++ App.js
@@ -8,17 +8,32 @@ import {
 import MainScreen from "./screens/MainScreen";
 import DetailScreen from "./screens/DetailScreen";
 import WriteScreen from "./screens/WriteScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const BaseNavi = createBottomTabNavigator({
   MainScreen: {
     screen: MainScreen
   },
   DetailScreen: {
-    screen: DetailScreen
+    screen: DetailScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons
+          name="calender-multiselect"
+          size={30}
+          style={{ color: tintColor }}
+        />
+      )
+    }
   },
   WriteScreen: {
     screen: WriteScreen
   }
+
+  /* 아이콘만 보여주고 싶을 때  */
+  // tabBarOptions: {
+  //   showLabel: false
+  // }
 });
 
 const MyNavi = createAppContainer(BaseNavi);
