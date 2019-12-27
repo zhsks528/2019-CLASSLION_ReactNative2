4161c1c4449e91bfcf1872902c4d9396cf519c8b
zhsks528 <hsm950825@naver.com>
2019-12-27 20:23:22 +0900

    NullPage Components 생성

---
+++ components/NullPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function NullPage() {
+  return (
+    <View style={styles.nullbox}>
+      <Text style={styles.nulltext}>일기를 선택해주세요</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  nullbox: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  nulltext: {
+    fontSize: 30,
+    color: "lightgrey"
+  }
+});
